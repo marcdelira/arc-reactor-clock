603d8f433e97697cc1ef8934dfee6dad33369539
Marcello de Lira Meira <marcello.meira@serpro.gov.br>
2023-09-07 14:39:05 -0300

    Primeiro commit

---
+++ include/DateTimeDisplay.h
@@ -0,0 +1,10 @@
+#ifndef DATETIMEDISPLAY_H
+#define DATETIMEDISPLAY_H
+
+#include <TM1637Display.h>
+#include <Arduino.h>
+
+void initDateTime();
+void handleDateTimeDisplay();
+
+#endif //DATETIMEDISPLAY_H
---
+++ include/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+  #define MAIN_H
+
+  #include <Arduino.h>
+  #include "DateTimeDisplay.h"
+  // #include "ledring.h"
+  // #include "netconfig.h"
+  // #include "webserver.h"
+
+#endif //MAIN_h
